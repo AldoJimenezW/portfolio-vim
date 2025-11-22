299126954b82e359e8a9f166525c3002680fd5c3
AldoJimenezW <aldojimenezw@gmail.com>
2025-11-21 23:05:49 -0300

    Add CNAME for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   build: {
     target: 'esnext',
-    minify: 'terser',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vue-vendor': ['vue', 'vue-router', 'pinia']
-        }
-      }
-    }
-  }
+    minify: 'esbuild',
+    sourcemap: false
+  },
+  publicDir: 'public'
 })
